4771d9e5fac5ed20ec112fd9a8983837e1b31d93
adeelaashraf11 <adeelaashraf929@gmail.com>
2025-01-28 13:16:46 -0800

    Add files via upload

---
+++ javasript-1.js
@@ -0,0 +1,16 @@
+let age=25;
+
+if ( age > 0 & age <= 12{
+    console.log( " you are a child.");
+}) else if (age >=13 & age <=19){
+    console.log ("you are a teenager");
+} else if ( age >=20 & age >=35){
+    console.log (" adult");
+} else if ( age >=36 & <= 60) {
+    console.log (" you are in midle age");
+}  else if ( age > 60){
+    console.log (" you are a senior");
+}  else { 
+    console.log (" invalid age"):
+}
+you are an adult
